9bfb0929810f0faa45dcb9dd8ae0b17fea1b363d
Matt Casanova <lazersquad@gmail.com>
2018-09-15 12:31:29 -0700

    Added first example of react and typescript

---
+++ react-typescript/src/FirstComponent.tsx
@@ -0,0 +1,19 @@
+
+import * as React from "react";
+
+let Logo ="https://logrocket.com/img/logo.png";
+
+export default class FirstComponent extends React.Component <{}> {
+    render() {
+        return (
+            <div>
+                {/* React components must have a wrapper node/element */}
+                <h1>A Simple React Component Example with Typescript</h1>
+                <div>
+                    <img src={Logo} /> 
+                </div>
+                <p>I am a component which shows the logrocket logo. For more info on Logrocket, please visit https://logrocket.com</p>
+            </div>
+        );
+    }
+}
