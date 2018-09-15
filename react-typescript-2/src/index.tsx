6dc5b3e0923528a6d134c75defc3acad02677e5e
Matt Casanova <lazersquad@gmail.com>
2018-09-15 12:57:52 -0700

    Added link to blog

--- react-typescript-2/src/index.tsx
+++ react-typescript-2/src/index.tsx
@@ -1,7 +1,10 @@
+https://www.typescriptlang.org/docs/handbook/react-&-webpack.html
+
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import { Hello } from "./components/Hello";
+
 
 ReactDOM.render(<Hello compiler="TypeScript" framework="React" />, 
     document.getElementById("example"));
