586a7703bb6105c4ca8df2dfce76674c68fff0d2
Matt Casanova <lazersquad@gmail.com>
2018-09-15 12:57:11 -0700

    Completed Demo

---
+++ react-typescript-2/src/components/Hello.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+export interface HelloProps { 
+    compiler: string; 
+    framework: string; 
+}
+
+/*
+export const Hello = (props: HelloProps) => {
+    return ( 
+        <h1>Hello from {props.compiler} and {props.framework}!</h1>
+    )
+};
+*/
+// 'HelloProps' describes the shape of props.
+// State is never set so we use the '{}' type.
+export class Hello extends React.Component<HelloProps, {}> {
+    public render(): JSX.Element {
+        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+    }
+}
